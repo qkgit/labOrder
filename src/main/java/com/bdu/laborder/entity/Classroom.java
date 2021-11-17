b504086cf8791b0d9140a3b27a1884cadc0b8c14
qkgit <1584546760@qq.com>
2021-11-17 18:06:10 +0800

    添加教室实体

---
+++ src/main/java/com/bdu/laborder/entity/Classroom.java
@@ -0,0 +1,12 @@
+package com.bdu.laborder.entity;
+
+import com.bdu.laborder.common.core.domain.BaseEntity;
+
+/**
+ * @Title 教室实体表
+ * @Author Qi
+ * @data 2021/11/16 15:56
+ */
+public class Classroom extends BaseEntity {
+
+}
