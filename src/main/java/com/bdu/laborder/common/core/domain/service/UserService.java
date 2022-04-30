c18822d7ee516707aa3c61a10923726658914f29
qkgit <1584546760@qq.com>
2022-04-30 21:31:26 +0800

    添加判断用户角色方法

--- src/main/java/com/bdu/laborder/common/core/domain/service/UserService.java
+++ src/main/java/com/bdu/laborder/common/core/domain/service/UserService.java
@@ -23,4 +23,20 @@ public class UserService {
         }
         return false;
     }
+
+    public static boolean isTeacher(SysUser user) {
+        String[] roleIds = user.getRoleIds();
+        if (StringUtils.isNotEmpty(roleIds)){
+            return Arrays.asList(roleIds).contains(UserConstants.TEACHER_ROLE_ID);
+        }
+        return false;
+    }
+
+    public static boolean isStudent(SysUser user) {
+        String[] roleIds = user.getRoleIds();
+        if (StringUtils.isNotEmpty(roleIds)){
+            return Arrays.asList(roleIds).contains(UserConstants.STUDENT_ROLE_ID);
+        }
+        return false;
+    }
 }
